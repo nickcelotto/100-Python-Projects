14b8d4ce8c24ba4c973b4d1af22d7b3d72852b80
Nicholas Celotto <nickcelotto15@gmail.com>
2025-04-07 16:57:35 -0500

    day 2

--- day1.py
+++ day1.py
@@ -1,3 +1,5 @@
+# Day 1 Project: Band Name Generator
+
 print("Welcome to the Band Name Generator.")
 city_name = input("Which city did you grow up in?\n")
 pet = input("What is the name of your pet?\n")
