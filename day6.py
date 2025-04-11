b5733c7442a8833d17f7b2bcce6473128387bd33
Nicholas Celotto <nickcelotto15@gmail.com>
2025-04-11 14:01:27 -0500

    day 6

--- day6.py
+++ day6.py
@@ -7,7 +7,7 @@ def number_guessing_game():
     print("I'm thinking of a number between 1 and 20.")
 
     secret_number = random.randint(1, 20)  # Random number between 1 and 20
-    guess = None  # Initialize guess variable
+    guess = 0  # Initialize guess variable
 
     while guess != secret_number:
         guess = int(input("Enter your guess: "))
